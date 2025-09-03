73ee11bfbf20ef7baa88171bb6e1f9fb10b7f8ee
Sezer Karaoğlan <szrkrgln007@gmail.com>
2025-09-03 04:58:32 +0300

    start_panel.sh değişikliği eklendi, gereksiz dosya kaldırıldı

--- start_panel.sh
+++ start_panel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Amına koduğum root olarak çalışınca sapıtıyor --user parametreside problemli
-source /home/sezer/panel/venv/bin/activate
-
+source venv/bin/activate
 # Steam ve Dc için GUI parametresi
 export DISPLAY=:0
 export XAUTHORITY=/home/sezer/.Xauthority
